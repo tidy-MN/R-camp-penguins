e63695d3bccc5a16c00a0499f6c1d790aed4f202
Festive Llama <dKvale@users.noreply.github.com>
2025-10-22 08:42:16 -0500

    Add files via upload

---
+++ public/page/stats/Introductory_Applied_Statistics_Practice.R
@@ -0,0 +1,37 @@
+# Introductory Applied Statistics Practice
+
+# load the required packages. Use install.packages() function if they have not been installed yet
+library(tidyverse)
+library(ggplot2)
+
+# set your working directory
+setwd("C:/Work/MDH_R_Course")
+
+# read in the cancer incidence data
+data <- read.csv("cancer_incidence_data.csv")
+
+# Breakout Session 1: 
+
+# Explore the cancer incidence dataset and identify some questions you
+# could try to answer with the available data. Use some of the statistical tests 
+# we covered (T-tests, Mann-Whitney U Test, Wilcoxon Signed Rank Test,
+# ANOVA, Chi-Squared Test, Correlation) along with any relevant visualizations
+# to answer the questions you identified. Discuss the tests you conducted
+# and your findings within the breakout session group. Be ready to share
+# some of your findings once we reconvene from the breakout sessions.  
+
+
+
+
+# Breakout Session 2: 
+
+# Continue exploring the dataset, this time focusing on linear regression.
+# Try building several models with various parameters, and make sure to
+# interpert the results and coefficients. Discuss the models you selected and
+# your findings with the rest of the breakout session group. Be ready to share
+# some of your findings once we reconvene from the breakout sessions.
+
+
+
+
+
